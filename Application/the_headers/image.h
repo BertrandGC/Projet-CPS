0e8bb637c56525f2c7d60e647cf1218a1287ce4e
Bertrand Gilbert-Collet <bertrand.gc@gmail.com>
2018-03-15 12:20:11 +0100

    debut fscanf

--- Application/the_headers/image.h
+++ Application/the_headers/image.h
@@ -8,5 +8,9 @@ typedef struct{
   uint32_t largeur;
   uint32_t hauteur;
   uint16_t val_max;
-  uint64_t data[largeur * hauteur];
+  uint64_t *data;
 }image;
+
+image init_file(char* path);
+
+// image init_std();
