79e6d4f58cb519f9e4f00da977a53ff01bad9a6b
Bertrand Gilbert-Collet <bertrand.gc@gmail.com>
2018-03-15 12:45:57 +0100

    fin seance 1

--- Application/the_sources/image.c
+++ Application/the_sources/image.c
@@ -1,19 +1,31 @@
 #include "image.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 image init_file(char *path) {
   FILE *file = fopen(path, "r");
+  image img;
+
   if (file != NULL) {
-    image img;
-    char *type;
+    char type[2];
     fscanf(file, "%s %u %u %hu", type, &img.largeur, &img.hauteur,
            &img.val_max);
+    img.data = malloc(img.largeur * img.hauteur * sizeof(uint64_t));
+    printf("%s %u %u %hu\n", type, img.largeur, img.hauteur, img.val_max);
+    while (fscanf(file) != EOF) {
+      /* code */
+    }
   } else {
     printf("Impossible d'ouvrir le fichier\n");
   }
+
+  fclose(file);
+  return img;
 }
 
 int main(int argc, char const *argv[]) {
-  /* code */
+
+  image img = init_file("../../Exemples/chalet.ppm");
+
   return 0;
 }
