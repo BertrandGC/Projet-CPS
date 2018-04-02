9692174ba1c2204963d041dc3c9da757e459c519
Bertrand Gilbert-Collet <bertrand.gc@gmail.com>
2018-04-02 12:21:42 +0200

    lecture fichier

--- Application/the_sources/image.c
+++ Application/the_sources/image.c
@@ -1,6 +1,19 @@
 #include "image.h"
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+unsigned int dtobin(unsigned char h) {
+  double n;
+  unsigned int b = 0;
+
+  for (n = 0; n <= 7; n++) {
+    b += (pow(10, n) * (h % 2));
+    h /= 2;
+  }
+
+  return b;
+}
 
 image init_file(char *path) {
   FILE *file = fopen(path, "r");
@@ -11,10 +24,34 @@ image init_file(char *path) {
     fscanf(file, "%s %u %u %hu", type, &img.largeur, &img.hauteur,
            &img.val_max);
     img.data = malloc(img.largeur * img.hauteur * sizeof(uint64_t));
-    printf("%s %u %u %hu\n", type, img.largeur, img.hauteur, img.val_max);
-    while (fscanf(file) != EOF) {
-      /* code */
+    // printf("%s %u %u %hu\n", type, img.largeur, img.hauteur, img.val_max);
+    int i = 0;
+    unsigned int bleu;
+    unsigned int vert;
+    uint64_t pixel;
+    while (!feof(file)) {
+      printf("//////////////////////////////////////////\n");
+      pixel = 0;
+      fscanf(file, "%lu %u %u", &pixel, &vert, &bleu);
+      printf("vert = %u\n", vert);
+      printf("bleu = %u\n", bleu);
+      printf("pixel = rouge: %lu\n", pixel);
+      pixel = pixel << 16;
+      printf("premier décalage: %lu\n", pixel);
+      pixel += vert;
+      printf("pixel = rouge et vert: %lu\n", pixel);
+      pixel = pixel << 16;
+      printf("second decalage: %lu\n", pixel);
+      pixel += bleu;
+      printf("pixel = RGB: %lu\n", pixel);
+
+      img.data[i] = pixel;
+      i++;
+      printf("//////////////////////////////////////////\n");
     }
+    printf("taille tab: %d\n", img.largeur * img.hauteur);
+    printf("i: %d\n", i);
+    printf("%lu\n", img.data[i]);
   } else {
     printf("Impossible d'ouvrir le fichier\n");
   }
